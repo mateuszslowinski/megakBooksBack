04dba0ec3daea5e8b4852ef454c2607514f62a0f
Mateusz <nowe7246@gmail.com>
2022-06-20 11:50:07 +0200

    Adding handle errors.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {json} from "express";
 import cors from 'cors';
 import "express-async-errors";
+import { handleError } from "./uttils/errors";
 
 
 const app = express();
@@ -9,6 +10,9 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(json());
+
+
+app.use(handleError);
 
 
 app.listen(3001, '0.0.0.0', () => {
