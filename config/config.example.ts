098fcc85ca24ee1222d62208dfaa6e5430f8b8ec
Mateusz <nowe7246@gmail.com>
2022-07-13 09:53:59 +0200

    Added config example and moved DB connection data into config.

---
+++ config/config.example.ts
@@ -0,0 +1,9 @@
+//
+// Example params to config:
+//
+// export const config = {
+//     dbHost: 'localhost',
+//     dbUser: 'user',
+//     dbPassword: 'password',
+//     dbDatabase: 'database',
+// }
