bc730b74bd16a2b3c9a98c609eaabacfdcc5e89f
Mateusz <nowe7246@gmail.com>
2022-09-10 15:06:37 +0200

    Added new validation function to book record.

--- records/book.record.ts
+++ records/book.record.ts
@@ -3,6 +3,7 @@ import {ValidationError} from "../uttils/errors";
 import {pool} from "../uttils/db";
 import {FieldPacket} from "mysql2";
 import {v4 as uuid} from 'uuid';
+import {validateLength, validateNumber} from "../uttils/validation";
 
 
 type BookRecordResults = [BookEntity[], FieldPacket[]];
@@ -29,28 +30,13 @@ export class BookRecord implements BookEntity {
         this.pages = obj.pages;
 
 
-        if (!obj.title || obj.title.length > 200) {
-            throw new ValidationError("Tytuł ksiąki nie może być pusty, ani dłuższy niż 200 znaków");
-        }
-
-        if (!obj.author || obj.author.length > 100) {
-            throw new ValidationError("Imię oraz nazwisko autora nie może być puste, ani być dłuższe niż 100 znaków");
-        }
-        if (!obj.rating || obj.rating > 10 || obj.rating < 0) {
-            throw new ValidationError("Ocena nie może być mniejsza niż 0 lub większa niż 10");
-        }
-        if (!obj.desc || obj.desc.length > 2000) {
-            throw new ValidationError("Opis nie może być pusty, ani większy niż 2000 znakóws");
-        }
-        if (!obj.publisher || obj.publisher.length > 100) {
-            throw new ValidationError("Nazwa wydawacy nie może być pusta, ani większa niż 100 znaków");
-        }
-        if (!obj.pages || obj.pages > 5472 || obj.pages < 1) {
-            throw new ValidationError("Liczba stron nie może być większa niż 5 472 i mniejsza niż 2");
-        }
-        if (!obj.species || obj.species.length > 36) {
-            throw new ValidationError("Gatunek literacki nie może być pusty, ani większa niż 36 znaków");
-        }
+        validateLength(obj.title, 200, 'Tytuł ksiąki nie może być pusty, ani dłuższy niż 200 znaków');
+        validateLength(obj.author, 100, "Imię oraz nazwisko autora nie może być puste, ani być dłuższe niż 100 znaków");
+        validateNumber(obj.rating, 0, 10, "Ocena nie może być mniejsza niż 0 lub większa niż 10");
+        validateLength(obj.desc, 2000, "Opis nie może być pusty, ani większy niż 2000 znaków");
+        validateLength(obj.publisher, 100, "Nazwa wydawacy nie może być pusta, ani większa niż 100 znaków");
+        validateNumber(obj.pages, 1, 5472,"Liczba stron nie może być większa niż 5 472 i mniejsza niż 2");
+        validateLength(obj.species, 36, "Gatunek literacki nie może być pusty, ani większa niż 36 znaków");
     }
 
     static async getOne(id: string): Promise<BookRecord> | null {
@@ -63,7 +49,6 @@ export class BookRecord implements BookEntity {
     }
 
     static async findAll(): Promise<BookRecord[]> {
-
         const [results] = await pool.execute("SELECT * FROM `books`") as BookRecordResults;
         return results.map(obj => new BookRecord(obj));
     }
@@ -72,7 +57,7 @@ export class BookRecord implements BookEntity {
         if (!this.id) {
             this.id = uuid();
         } else {
-            throw new ValidationError('Cannot insert something that is already inserted');
+            throw new ValidationError('Nie można dodać coś, co już istnieje');
         }
         await pool.execute('INSERT INTO `books`(`id`,`title`,`author`,`rating`,`desc`,`publisher`,`species`,`pages`) VALUES (:id,:title,:author,:rating,:desc,:publisher,:species,:pages)', this)
 
@@ -81,7 +66,7 @@ export class BookRecord implements BookEntity {
 
     async remove(): Promise<void> {
         if (!this.id) {
-            throw new ValidationError('This book does not exist')
+            throw new ValidationError('Ta książka nie istnieje')
         }
         await pool.execute('DELETE FROM `books` WHERE `id` =:id', {
             id: this.id,
@@ -109,7 +94,7 @@ export class BookRecord implements BookEntity {
         return results.map(obj => new BookRecord(obj));
     }
 
-    static async titleAlphabetically():Promise<BookRecord[]> {
+    static async titleAlphabetically(): Promise<BookRecord[]> {
         const [results] = await pool.execute('SELECT * FROM `books` ORDER BY `title`') as BookRecordResults
         return results.map(obj => new BookRecord(obj));
     }
