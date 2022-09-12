0a5e20647ad2e1eba5810d091de12c6eb9c689b6
Mateusz <nowe7246@gmail.com>
2022-09-12 13:39:58 +0200

    Add export user type for frontend.

--- types/user/user.entity.ts
+++ types/user/user.entity.ts
@@ -7,6 +7,25 @@ export interface UserEntity {
     name: string;
     email: string;
     password: string;
+    token:string;
     isAdmin: boolean;
     createdAt: Date;
+}
+
+
+export interface LoginUser {
+    email: string;
+    password: string;
+}
+
+export interface LoginUserResponse {
+    name: string,
+    email: string,
+    isAdmin: boolean,
+    token: string;
+}
+
+export interface LogoutUserResponse {
+    ok?: string;
+    error?: string;
 }
